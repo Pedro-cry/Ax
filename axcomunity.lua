f72e9fc0166fc8839f17b30a02e77ce673aaa710
Pedro-cry <snowffzx@gmail.com>
2024-07-30 14:12:36 -0300

    Update axcomunity.lua

--- axcomunity.lua
+++ axcomunity.lua
@@ -33,10 +33,22 @@ local MainSection = MainTab:CreateSection("Main")
 local RollbackButton = MainTab:CreateButton({
     Name = "Rollback",
     Callback = function()
+            local Toggle = MainTab:CreateToggle({
+   Name = "Toggle Example",
+   CurrentValue = false,
+   Flag = "Toggle1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+   Callback = function(Value)
+   -- The function that takes place when the toggle is pressed
+   -- The variable (Value) is a boolean on whether the toggle is true or false
     end,
 })
 
 local MainTab = Window:CreateTab("🗡️ Farm", nil)
 local MainSection = MainTab:CreateSection("Farm")
+            local RollbackButton = FarmTab:CreateButton({
+    Name = "Rollback",
+    Callback = function()
+                    end,
+                })
 
 

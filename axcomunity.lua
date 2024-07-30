b7d8d28431903c129e5891a75bdd54aac03847b5
Pedro-cry <snowffzx@gmail.com>
2024-07-30 13:22:40 -0300

    Update axcomunity.lua

--- axcomunity.lua
+++ axcomunity.lua
@@ -1,53 +1,27 @@
--- Carregando a Biblioteca MercuryLib (substitua pelo link correto)
-local Mercury = loadstring(game:HttpGet("https://raw.githubusercontent.com/deeeity/mercury-lib/master/src.lua"))()
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
--- Criando a GUI
-local GUI = Mercury:Create{
-    Name = "MyInterface",
-    Size = UDim2.fromOffset(600, 400),
-    Theme = Mercury.Themes.Dark,
-    Link = "https://github.com/deeeity/mercury-lib"
-}
-
--- Criando uma guia (tab)
-local Tab = GUI:Tab{
-    Name = "MyTab",
-    Icon = "rbxassetid://8569322835" -- Substitua pelo ID do ícone desejado
-}
-
--- Adicionando um botão à guia
-local autoClickButton = Tab:Button{
-    Name = "Auto Clicker: Desativado",
-    Description = "Ative ou desative o auto clicker",
-    Callback = function()
-        autoClicking = not autoClicking
-        if autoClicking then
-            autoClickButton:SetText("Auto Clicker: Ativado")
-            autoRightClick() -- Inicia o auto clicker
-        else
-            autoClickButton:SetText("Auto Clicker: Desativado")
-        end
-    end
-}
-
--- Adicionando evento de clique com o botão direito
-autoClickButton.MouseButton2Click:Connect(function()
-    autoClicking = not autoClicking
-    if autoClicking then
-        autoClickButton:SetText("Auto Clicker: Ativado")
-        autoRightClick() -- Inicia o auto clicker
-    else
-        autoClickButton:SetText("Auto Clicker: Desativado")
-    end
-end)
-
--- Adicionando evento de toque (clique com o dedo)
-autoClickButton.TouchTap:Connect(function()
-    autoClicking = not autoClicking
-    if autoClicking then
-        autoClickButton:SetText("Auto Clicker: Ativado")
-        autoRightClick() -- Inicia o auto clicker
-    else
-        autoClickButton:SetText("Auto Clicker: Desativado")
-    end
-end)
+local Window = Rayfield:CreateWindow({
+   Name = "Anime strike simulator",
+   LoadingTitle = "Ax Community",
+   LoadingSubtitle = "by Ax",
+   ConfigurationSaving = {
+      Enabled = false,
+      FolderName = nil, -- Create a custom folder for your hub/game
+      FileName = "Ax Hub"
+   },
+   Discord = {
+      Enabled = true,
+      Invite = "EuGGNvkZ", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ABCD would be ABCD
+      RememberJoins = true -- Set this to false to make them join the discord every time they load it up
+   },
+   KeySystem = true, -- Set this to true to use our key system
+   KeySettings = {
+      Title = "Anime Strike Simulator",
+      Subtitle = "Link In Discord Server",
+      Note = "Join Server From Misc Tab",
+      FileName = "AxHub", -- É recomendado usar algo exclusivo, pois outros scripts que usam Rayfield podem substituir seu arquivo de chave
+      SaveKey = true, -- A chave do usuário será salva, mas se você alterar a chave, ele não poderá usar seu script
+      GrabKeyFromSite = true, -- Se isso for verdade, defina Key abaixo para o site RAW do qual você gostaria que Rayfield obtivesse a chave
+      Key = {"https://raw.githubusercontent.com/Pedro-cry/Ax/main/AxHub"} -- Lista de chaves que serão aceitas pelo sistema, podem ser links de arquivos RAW (pastebin, github etc) ou strings simples ("hello","key22")
+   }
+})

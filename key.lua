d26395bbafb0ec361c12a0ef104d1e4e5d2c1ed3
Pedro-cry <snowffzx@gmail.com>
2024-07-31 00:39:21 -0300

    Update key.lua

--- key.lua
+++ key.lua
@@ -1,1 +1,9 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/MaGiXxScripter0/keysystemv2api/master/setup.lua"))()
+local KeySystem = _G.KSS.classes.keysystem.new("your_application_name") -- Create class KeySystem
+local Key = KeySystem:key('key_user') -- Create class Key
 
+if Key.finish and Key:verifyHWID() then
+  print("Good")
+else
+  print("Bad")
+end```

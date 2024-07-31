9f425a8dc5fbab55e33a999f3376f760926eb4b5
Pedro-cry <snowffzx@gmail.com>
2024-07-31 00:45:36 -0300

    Update key.lua

--- key.lua
+++ key.lua
@@ -1,9 +1,19 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/MaGiXxScripter0/keysystemv2api/master/setup.lua"))()
-local KeySystem = _G.KSS.classes.keysystem.new("your_application_name") -- Create class KeySystem
-local Key = KeySystem:key('key_user') -- Create class Key
+local HashLibrary = loadstring(game.HttpGet("https://raw.githubusercontent.com/Egor-Skriptunoff/pure_lua_SHA/master/sha2.lua"))()
+local KeyLibrary = loadstring(game.HttpGet("https://raw.githubusercontent.com/MaGiXxScripter0/keysystemv2api/master/version2_1.lua"))()
 
-if Key.finish and Key:verifyHWID() then
-  print("Good")
+KeyLibrary.Set({
+    ApplicationName = "app_name",
+    AuthType = "clientid",
+    EncryptionKey = "any data",
+    TrueData = "any data",
+    FalseData = "any data",
+})
+
+local o_data = KeyLibrary.VerifyKey("keystring")
+local _, decrypted_data = KeyLibrary.XORDecode(o_data)
+local newhash = HashLibrary.sha1("YourTrueData")
+if newhash == decrypted_data then
+    warn("Key is a valid key !")
 else
-  print("Bad")
-end```
+    warn("Key is non-valid !")
+end
